999a59b491fe240c4d7306a5b4e75c8f5244e435
Yggdrasil80 <louisdechorivit@gmail.com>
2022-08-09 22:37:23 +0200

    :card_file_box: Introduce MongoDB support through Storage abstraction layer

---
+++ storage/database.go
@@ -0,0 +1,11 @@
+package storage
+
+type Database interface {
+	Aggregate(pipeline Pipeline) (*Cursor, error)
+	CreateCollection(collection string) error
+	CreateView(view string, viewOn string, pipeline Pipeline) error
+	Drop() error
+	ListCollectionNames(filter Filter) ([]string, error)
+	ListCollections(filter Filter) (*Cursor, error)
+	Name() string
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,24 @@
 package storage
 
 type Storage interface {
-	Get(name string) (string, error)
-	Set(name string, value string) error
+	SetDatabase(database string) error
+	SetCollection(collection string) error
+	CreateCollection(collection string) error
+
+	Database
+	Collection
+}
+
+type Filter string
+type Pipeline string
+
+type Cursor interface {
+	Current() []byte
+	All(results interface{}) error
+	Close() error
+	Decode(result interface{}) error
+	Err() error
+	ID() int64
+	Next() bool
+	TryNext() bool
 }
