85a46548d66c2f3a419e20a66dc770ccd15c5bca
Yggdrasil80 <louisdechorivit@gmail.com>
2022-08-11 01:41:45 +0200

    :bug: Fix mistake with interfaces pointers in Cursor

--- storage/collection.go
+++ storage/collection.go
@@ -1,7 +1,7 @@
 package storage
 
 type Collection interface {
-	AggregateCollection(pipeline Pipeline) (*Cursor, error)
+	AggregateCollection(pipeline Pipeline) (Cursor, error)
 
 	CountDocuments(filter Filter) (int64, error)
 	EstimatedDocumentCount() (int64, error)
@@ -11,20 +11,20 @@ type Collection interface {
 
 	Distinct(field string, filter Filter) ([]interface{}, error)
 
-	Find(filter Filter) (*Cursor, error)
-	FindOne(filter Filter) (*Cursor, []byte, error)
-	FindOneAndDelete(filter Filter) (*Cursor, []byte, error)
-	FindOneAndReplace(filter Filter, replacement interface{}) (*Cursor, []byte, error)
-	FindOneAndUpdate(filter Filter, update interface{}) (*Cursor, []byte, error)
+	Find(filter Filter) (Cursor, error)
+	FindOne(filter Filter) (Cursor, []byte, error)
+	FindOneAndDelete(filter Filter) (Cursor, []byte, error)
+	FindOneAndReplace(filter Filter, replacement interface{}) (Cursor, []byte, error)
+	FindOneAndUpdate(filter Filter, update interface{}) (Cursor, []byte, error)
 
-	InsertMany(documents []interface{}) ([]interface{}, error)
 	InsertOne(document interface{}) (interface{}, error)
+	InsertMany(documents []interface{}) ([]interface{}, error)
 
 	ReplaceOne(filter Filter, replacement interface{}) (int64, int64, int64, error)
 
-	UpdateByID(id interface{}, update interface{}) (int64, int64, int64, error)
-	UpdateMany(filter Filter, update interface{}) (int64, int64, int64, error)
 	UpdateOne(filter Filter, update interface{}) (int64, int64, int64, error)
+	UpdateMany(filter Filter, update interface{}) (int64, int64, int64, error)
+	UpdateByID(id interface{}, update interface{}) (int64, int64, int64, error)
 
 	CollectionName() string
 	DropCollection() error
--- storage/database.go
+++ storage/database.go
@@ -1,13 +1,13 @@
 package storage
 
 type Database interface {
-	AggregateDatabase(pipeline Pipeline) (*Cursor, error)
+	AggregateDatabase(pipeline Pipeline) (Cursor, error)
 
 	CreateCollection(collection string) error
 	CreateView(view string, viewOn string, pipeline Pipeline) error
 
 	ListCollectionNames(filter Filter) ([]string, error)
-	ListCollections(filter Filter) (*Cursor, error)
+	ListCollections(filter Filter) (Cursor, error)
 
 	DatabaseName() string
 	DropDatabase() error
