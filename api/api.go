ca3a8c468fe80063b8a4f784628fb74f979cd908
Yggdrasil80 <louisdechorivit@gmail.com>
2022-08-10 23:11:03 +0200

    :sparkles: Introduce logger

--- api/api.go
+++ api/api.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"github.com/Yggdrasil80/home-plugin-api/event"
+	"github.com/Yggdrasil80/home-plugin-api/logger"
 	"github.com/Yggdrasil80/home-plugin-api/storage"
 	"github.com/labstack/echo/v4"
 )
 
 type Api struct {
+	Logger   logger.Logger
 	EventBus event.Bus
 	Storage  storage.Storage
 
---
+++ logger/logger.go
@@ -0,0 +1,8 @@
+package logger
+
+type Logger interface {
+	Info(message string, args ...interface{})
+	Debug(message string, args ...interface{})
+	Warn(message string, args ...interface{})
+	Error(message string, args ...interface{})
+}
